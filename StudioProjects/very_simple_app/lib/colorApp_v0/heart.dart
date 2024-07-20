e84ed95a1a41a6e038218ebfd308b0d003f1bfa4
Lana <lana100705@icloud.com>
2024-07-21 00:42:08 +0300

    ColorChangingApp

---
+++ StudioProjects/very_simple_app/lib/colorApp_v0/heart.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Heart extends StatefulWidget {
+  const Heart({super.key});
+
+  @override
+  State<Heart> createState() {
+    return _HeartState();
+  }
+}
+
+class _HeartState extends State<Heart> {
+  Color heartColor = Colors.pink;
+
+  void updateColor() {
+    final random = Random();
+    final int min = 1;
+    final int max = 255;
+    setState(() {
+      heartColor = Color.fromARGB(
+          min + random.nextInt(max - min + 1),
+          min + random.nextInt(max - min + 1),
+          min + random.nextInt(max - min + 1),
+          min + random.nextInt(max - min + 1));
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Stack(
+            alignment: Alignment.center,
+            children: [
+          Icon(
+            size: 310,
+            Icons.favorite,
+            color: Colors.black,
+          ),
+          Icon(
+            size: 300,
+            Icons.favorite,
+            color: heartColor,
+          ),
+        ]),
+        ElevatedButton(onPressed: updateColor, child: Text("Change the colour"))
+      ],
+    );
+  }
+}
+
